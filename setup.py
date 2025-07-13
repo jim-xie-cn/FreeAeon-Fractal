588054678d27c7386d666a7558e397fa2054eba2
jim_xie <jim_xie@trendmicro.com>
2025-07-13 16:01:58 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(filename):
 
 setup(
     name="FreeAeon-Fractal",
-    version="0.2.6",
+    version="0.2.7",
     author="Jim Xie",
     author_email="jim.xie.cn@outlook.com",
     description="Toolkit for Multifractal Analysis",
